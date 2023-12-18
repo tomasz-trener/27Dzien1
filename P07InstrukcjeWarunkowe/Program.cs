571be3ce219e522eae8493ef71e147581d88a431
kurs <kurs@DESKTOP-V3HDKPQ>
2023-12-18 11:52:12 +0100

    q

---
+++ P07InstrukcjeWarunkowe/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07InstrukcjeWarunkowe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swój wzrost");
+            int wzrost = Convert.ToInt32(Console.Read());
+
+            // wypisz "wyoski" gdy wzrost wyższy niz 180 wpp. wypisz niski 
+
+            bool c1 = wzrost > 180;
+
+            if (c1)
+            {
+                Console.WriteLine("wysoki");
+            }
+            else
+            {
+                Console.WriteLine("niski");
+            }
+
+            if (c1) //wzrost > 180
+                Console.WriteLine("wysoki");
+            else
+                if(wzrost>160)
+                    Console.WriteLine("średni");
+                else
+                    Console.WriteLine("niski");
+
+            // gdy sie da to warto unikac zagniezdzen 
+
+            if(wzrost>180)
+                Console.WriteLine($"twój wzrost to {wzrost} i jest wysoki");
+            else if(wzrost>160)
+                Console.WriteLine($"twój wzrost to {wzrost} i jest średni");
+            else if (wzrost > 150)
+                Console.WriteLine($"twój wzrost to {wzrost} i jest niski");
+            else
+                Console.WriteLine($"twój wzrost to {wzrost} i jest bardzo niski");
+
+            // zapobieganie powtarzajacym sie kawalom kodu 
+            string raport;
+            if (wzrost > 180)
+                raport =  "wysoki";
+            else if (wzrost > 160)
+                raport = "średni";
+            else if (wzrost > 150)
+                raport = "niski";
+            else
+                raport = "bardzo niski";
+
+            raport = $"twój wzrost to {wzrost} i jest {raport}";
+
+
+            // operator tój-argumentowy 
+
+            string wyn;
+            if (wzrost > 180)
+                wyn = "wyoski";
+            else
+                wyn = "niski";
+
+            string wyn1 = wzrost > 180 ? "wysoki" : "niski";
+
+            string wynik2 = wzrost > 180 ? "wyoski" : wzrost < 160 ? "niski" : "średnmi";
+        }
+    }
+}
---
+++ P10Petle/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10Petle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+
+            while (k < 10)
+            {
+                k++;
+                Console.WriteLine(k);
+            }
+
+            Console.ReadKey();
+
+            // zastanwomy sie nad problemem z zadnia p04 
+
+            string rownanie = "10 200 400";
+                             //0123456
+
+            int pozycjaSpacji = -1;
+            int i = 0;
+            while (i< rownanie.Length)
+            {
+                if (rownanie.Substring(i, 1) == " ")
+                    pozycjaSpacji = i;
+                i++;
+            }
+
+            // to by algorytm ktory dziala jak indexOf ale znajduje ostatnia spacje 
+            // nasz algorytm to odpowiednik gunkcji LastIndexOf 
+            //rownanie.LastIndexOf()
+
+
+            //a co by bylo gdybysmy chcieli zrobic "first occeurance" 
+            pozycjaSpacji = -1;
+            i = 0;
+            while (i < rownanie.Length)
+            {
+                if (rownanie[i] == ' ' )
+                {
+                    pozycjaSpacji = i;
+                    break; // przerywa petle 
+                }
+                   
+                i++;
+            }
+
+
+            string napis = "ala ma kota";
+            char znak = 'n';
+
+
+        }
+    }
+}
