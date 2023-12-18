d5486138b30ecd3e238632d77933c243fd6cad2e
kurs <kurs@DESKTOP-V3HDKPQ>
2023-12-18 14:09:10 +0100

    q

--- P13ZadaniePetle/Program.cs
+++ P13ZadaniePetle/Program.cs
@@ -10,6 +10,32 @@ namespace P13ZadaniePetle
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wczytaj zdanie");
+            string zdanie = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("Wczytaj wyraz");
+                string wyraz = Console.ReadLine();
+
+                if (wyraz.ToLower() == "koniec")
+                    break;
+
+                int i = 0;
+                int licznik = 0;
+
+                while (i <= zdanie.Length - wyraz.Length)
+                {
+                    string podciag = zdanie.Substring(i, wyraz.Length);
+                    if (podciag == wyraz)
+                        licznik++;
+                    i++;
+                }
+
+                string raport = "Slowo/znak '{0}' wystepuje w podanym zdaniu {1} razy";
+                Console.WriteLine(string.Format(raport, wyraz, licznik));
+            }
+             
         }
     }
 }
