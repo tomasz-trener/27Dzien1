575ed7a2aaa056374def86fef2385c68c6a9cb1e
kurs <kurs@DESKTOP-V3HDKPQ>
2023-12-18 11:00:27 +0100

    q

---
+++ P05Debugowanie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05Debugowanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // F10 przechodzimy do kolejnej instrukcji 
+            string napis = "ala ma kota";
+
+            int a = 10;
+            int b = 5;
+
+            b = b * 2;
+            b++;
+            a = b + napis.Length;
+
+
+        }
+    }
+}
---
+++ P06Operatory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06Operatory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1; // operator przypisania 
+            bool czy = a == 1; // == operator porównania 
+
+            a = a + 1;
+            a++; // zwieksz o 1 
+
+            a += 5; // zwieksz np. o 5 
+
+            a *= 2;// przemnoz przez 2 czyli to samo co a=a*2
+
+            a -= 6;// zmniejsz o 6 
+
+            a /= 2; // przedziel przez 2 
+
+            bool czyMniejsze = a < 5; // czy mniejsze od...
+            bool czyWiekszeLubRowne = a >= 5; // czy wieksze lub równe ...
+
+            bool b1 = a < 5 || a > 10; // czy a jest mniejsze od 5 lub wieksze od 10
+            bool b2 = a > 5 && a < 10; // i 
+
+            bool b3 = a != 5; // różne od 
+
+            bool b4 = !(a == 5); // ! to zaprzeczenie 
+
+
+        }
+    }
+}
